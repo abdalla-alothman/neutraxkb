fe7371c2908c53cd1a2efc67608cb79a0f1ee25a
Abdalla Alothman <asa.alothman@gmail.com>
2014-07-31 20:14:06 +0300

    setup.py modification

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='neutraxkb',
     author_email='abdallah.alothman@gmail.com',
     license='GPL',
     description='NeutraXkbSwitch is a system tray tool to switch between user defined keyboard layouts. The tool requires any panel with a system tray enabled. It further requires PyQt4.',
-    packages=find_packages(),
-    scripts = ['neutraswitch', 'neutrakb.py', 'neutraxkbconfig.py', 'nxkbcfgparser.py', 'XkbConfig_rc.py', 'XkbConfig_ui.py'],
+    py_modules=['neutraxkbconfig', 'nxkbcfgparser', 'XkbConfig_rc', 'XkbConfig_ui'],
+    scripts = ['neutraswitch', 'neutrakb.py'],
     )
